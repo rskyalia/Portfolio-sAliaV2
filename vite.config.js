5ce92fc6696cbb375580487f9a234295c5751167
rskyalia <syahbanialif79@gmail.com>
2026-07-02 14:06:31 +0700

    baruu buat vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio-sAliaV2/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
